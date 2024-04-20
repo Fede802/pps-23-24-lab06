96f04118abd34857d77a4ac6d2f564e28a091971
Federico Bravetti <federico.bravetti2@studio.unibo.it>
2024-04-20 13:22:47 +0200

    ex2 refactor

--- src/main/scala/ex2/ConferenceReviewing.scala
+++ src/main/scala/ex2/ConferenceReviewing.scala
@@ -20,26 +20,38 @@ object ConferenceReviewing:
   def apply(): ConferenceReviewing = ConferenceReviewingImpl()
   private case class ConferenceReviewingImpl() extends ConferenceReviewing:
     private var articles: List[(Int, Map[Question, Int])] = List()
+
+    private def articlesID: List[Int] = 
+      articles.map(_._1).distinct
+    private def averageScore(article: Int, question: Question): Double =
+      average(scores(article, question))
+    private def average[A: Numeric](l: List[A]): Double =
+      import Numeric.Implicits._
+      l.sum.toDouble / l.length
+    private def scores(article: Int, question: Question): List[Int] =
+      articlesWithID(article).map(_._2(question))
+    private def articlesWithID(article: Int): List[(Int, Map[Question, Int])] = 
+      articles.filter(_._1 == article)
+    private def weightedFinalScore(article: Int): List[Double] =
+      articlesWithID(article).map((i,q) => q(CONFIDENCE)*q(FINAL)/10.0)
     override def loadReview(article: Int, scores: Map[Question, Int]): Unit =
       articles ::= (article, scores)
     override def loadReview(article: Int, relevance: Int, significance: Int, confidence: Int, fin: Int): Unit =
-      loadReview(article, Map(RELEVANCE -> relevance, SIGNIFICANCE -> significance, CONFIDENCE -> confidence, FINAL -> fin))
+      loadReview(
+        article, 
+        Map(RELEVANCE -> relevance, SIGNIFICANCE -> significance, CONFIDENCE -> confidence, FINAL -> fin)
+      )
     override def orderedScores(article: Int, question: Question): List[Int] =
       scores(article,question).sorted
     override def averageFinalScore(article: Int): Double =
       averageScore(article, FINAL)
-    private def averageScore(article: Int, question: Question): Double =
-      average(articles.filter(_._1 == article).map(_._2(question)))
-    private def average(l: List[Double]): Double =
-      l.sum / l.length
-    private def scores(article: Int, question: Question): List[Int] =
-      articles.filter(_._1 == article).map(_._2(question))
     override def acceptedArticles(): Set[Int] =
-      articles.map(_._1).toSet.filter(accepted)
+      articlesID.filter(accepted).toSet
     override def sortedAcceptedArticles(): List[(Int, Double)] =
-      articles.map(_._1).distinct.collect({case a if accepted(a) => (a,averageFinalScore(a))}).sortBy(_._2)
+      articlesID.collect({case a if accepted(a) => (a,averageFinalScore(a))}).sortBy(_._2)
     private def accepted(a: Int): Boolean =
       averageFinalScore(a) > 5 && scores(a, RELEVANCE).exists(_ >= 8)
     override def averageWeightedFinalScoreMap(): Map[Int, Double] =
-      articles.map(_._1).distinct.map(a => (a, average(articles.filter(_._1 == a).map((i,q) => q(CONFIDENCE)*q(FINAL)/10.0)))).toMap
+      articlesID.map(a => (a, average(weightedFinalScore(a)))).toMap
+
 
